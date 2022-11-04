9a58d127d1fc63f6949bd70693d718fc2cbd7a0d
TouHunp <114973441+TouHunp@users.noreply.github.com>
2022-11-04 20:31:42 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-List1 = [8, 9, 3, 6, 8, 9, 10]
-List1.reverse
-print("The reversed list is", List1)
-
-List2 = [8, 9, 3, 6, 23, 15, 10, 66, 59, 32, 46]
-List2.sort
-print("The sort list is", List2)
